ee5e6deae481ec227f862ca4c2816346c7558f01
Aryagithubk <aryasanya55@gmail.com>
2025-04-22 02:17:32 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,10 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "client", "build")));
 
+  app.get("/", (req, res)=>{
+    res.send("Welcome to Educart Backend. Server is successfully running....")
+  })
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
